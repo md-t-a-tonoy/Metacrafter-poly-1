b3cd9d68e0e3dd254284d7946e719829868dd910
Md Thouhedul Alam Tonoy <117182746+md-t-a-tonoy@users.noreply.github.com>
2023-12-31 23:16:54 +0530

    getBalance.js

--- getBalance.js
+++ getBalance.js
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
 const tokenContractJSON = require("../artifacts/contracts/Alam.sol/Alam.json");
 
-const tokenAddress = "0x348342317E6CC9B05Df093794B408B27Cf2ADA2f"; // place your erc721 contract address here
+const tokenAddress = ""; // place your erc721 contract address here
 const tokenABI = tokenContractJSON.abi;
-const walletAddress = "0xc9c652DE83AE799549a6Fc0306cBd0eB5bc0899b"; // place your public address for your wallet here
+const walletAddress = ""; // place your public address for your wallet here
 
 async function main() {
 
